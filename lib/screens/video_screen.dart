821c52c9041ece48db7818a248ccfbf096836b11
aryachtama <aryachtama@gmail.com>
2025-08-16 19:15:48 +0700

    Create video_screen.dart

---
+++ lib/screens/video_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VideoScreen extends StatelessWidget {
+  const VideoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Video Call")),
+      body: const Center(child: Text("Video feature coming soon...")),
+    );
+  }
+}
