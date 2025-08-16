752d5aae726bcb78e6bccc8a72dab372890a976e
aryachtama <aryachtama@gmail.com>
2025-08-16 19:14:06 +0700

    Update profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -3,9 +3,22 @@ import 'package:lottie/lottie.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text('Profile')),
-    body: Center(child: Lottie.asset('assets/animations/mascot.json', width: 120)),
-  );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Profile")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('assets/animations/mascot.json', width: 120),
+            const SizedBox(height: 20),
+            const Text("Memoa User",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
 }
