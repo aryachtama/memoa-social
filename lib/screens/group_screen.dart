c9d48c134adbf0c65f9c2d6a0fcd97530ba694c4
aryachtama <aryachtama@gmail.com>
2025-08-16 19:18:11 +0700

    Create group_screen.dart

---
+++ lib/screens/group_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GroupScreen extends StatelessWidget {
+  const GroupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Group Discussion")),
+      body: const Center(child: Text("Group discussion feature coming soon...")),
+    );
+  }
+}
